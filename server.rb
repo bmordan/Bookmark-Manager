1e42bf07d9d96f3556de05e691b378788547e219
Bernard Mordan <bernardmordan@gmail.com>
2014-10-07 15:55:55 +0100

    User model added and simple auth set up and passing

--- server.rb
+++ server.rb
@@ -5,11 +5,14 @@ class BookmarkManager < Sinatra::Base
 
   env = ENV["RACK_ENV"] || "development"
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-  require './lib/link'
-  require './lib/tag'
+  Dir["./lib/*"].each { |file| require file }
   DataMapper.finalize
   DataMapper.auto_upgrade!
 
+  set :public_folder, 'public'
+  enable :sessions
+  set :session_secret, 'super secret'
+  
   get '/' do
     @links = Link.all
     erb :index
@@ -26,8 +29,21 @@ class BookmarkManager < Sinatra::Base
   get '/tags/:text' do
     tag = Tag.first(:text => params[:text])
     @links = tag ? tag.links : []
-    puts tag.links.inspect
     erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email],:password => params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  end
+
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
   end
 
   run! if app_file == $0
---
+++ spec/listing_all_links_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature "User filters their links" do
+
+  before(:each) {
+    Link.create(:url => "http://www.makersacademy.com",
+                :title => "Makers Academy",
+                :tags => [Tag.first_or_create(:text => 'education')])
+    Link.create(:url => "http://www.google.com",
+                :title => "Google",
+                :tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.bing.com",
+                :title => "Bing",
+                :tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.code.org",
+                :title => "Code.org",
+                :tags => [Tag.first_or_create(:text => 'education')])
+  }
+  
+  scenario "filtered by a tag" do
+    visit '/tags/search'
+    expect(page).not_to have_content("Makers Academy")
+    expect(page).not_to have_content("Code.org")
+    expect(page).to have_content("Google")
+    expect(page).to have_content("Bing")
+  end
+
+end
