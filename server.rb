02da6d03ca5f6f32dc372d6e43ecc08b169782e6
Bernard Mordan <bernardmordan@gmail.com>
2014-10-07 08:39:09 +0100

    many to many save failing

--- server.rb
+++ server.rb
@@ -18,7 +18,8 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
+    tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
+    puts tags.inspect
     Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
