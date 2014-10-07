af90f13b84e83000c9061c262b7551c76d858a09
Bernard Mordan <bernardmordan@gmail.com>
2014-10-07 11:55:33 +0100

    All tests passing after database reset

--- server.rb
+++ server.rb
@@ -1,15 +1,15 @@
-env = ENV["RACK_ENV"] || "development"
-require 'data_mapper'
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-require './lib/link'
-require './lib/tag'
-DataMapper.finalize
-DataMapper.auto_upgrade!
-
 require 'sinatra/base'
-
+require 'data_mapper'
 
 class BookmarkManager < Sinatra::Base
+
+  env = ENV["RACK_ENV"] || "development"
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+  require './lib/link'
+  require './lib/tag'
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+
   get '/' do
     @links = Link.all
     erb :index
@@ -18,8 +18,9 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
-    puts tags.inspect
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
     Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
